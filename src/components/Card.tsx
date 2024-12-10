12e6d41338b58bb44a65c109c18e0ca4c3752090
Jaspreet <jaspreetkn92@gmail.com>
2024-12-10 19:25:38 +0000

    added CardProps

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,9 @@
-const Card = ({ children, bg = 'bg-gray-100' }) => {
+interface CardProps {
+  children: string;
+  bg?: string;
+}
+
+const Card = ({ children, bg = 'bg-gray-100' }: CardProps) => {
   return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>;
 };
 
