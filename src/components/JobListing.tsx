8bd38e2690e5c88852d425a855303738460a29a7
Jaspreet <jaspreetkn92@gmail.com>
2024-12-11 10:03:38 +0000

    added state to read more/less button for job description

--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 interface JobProps {
   type: string;
   title: string;
@@ -8,6 +10,12 @@ interface JobProps {
 }
 
 const JobListing = ({ job }: { job: JobProps }) => {
+  const [showMore, setShowMore] = useState(true);
+
+  const handleClick = () => {
+    setShowMore(!showMore);
+  };
+
   return (
     <div key={job.id} className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -16,9 +24,16 @@ const JobListing = ({ job }: { job: JobProps }) => {
           <h3 className="text-xl font-bold">{job.title}</h3>
         </div>
 
-        <div className="mb-5">{job.description}</div>
+        <div className="mb-5">
+          {showMore
+            ? job.description.substring(0, 90) + '...'
+            : job.description}
+        </div>
 
         <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
+        <button onClick={handleClick}>
+          {showMore ? 'Read More' : 'Read Less'}
+        </button>
 
         <div className="border border-gray-100 mb-5"></div>
 
