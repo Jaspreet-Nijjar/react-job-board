781875fb3eb5306f6853ac11d9cdd1817ea43dd7
Jaspreet <jaspreetkn92@gmail.com>
2024-12-12 09:39:43 +0000

    added icon to JobListing component

--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { FaMapMarker } from 'react-icons/fa';
 
 interface JobProps {
   type: string;
@@ -42,7 +43,7 @@ const JobListing = ({ job }: { job: JobProps }) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <i className="fa-solid fa-location-dot text-lg"></i>
+            <FaMapMarker className="inline text-lg mb-1 mr-1" />
             {job.location}
           </div>
           <a
