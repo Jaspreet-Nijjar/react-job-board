1e83107a8797723a53491b34f01da5a93599d1a6
Jaspreet <jaspreetkn92@gmail.com>
2024-12-12 10:01:01 +0000

    changed links using Link from react router

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import {
 } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Homepage />} />
+        <Route path="/jobs" element={<JobsPage />} />
       </Route>
     )
   );
