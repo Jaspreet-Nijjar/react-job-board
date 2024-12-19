35fe958b04222348bdbc6fa47b2afd6c64dfab0e
Jaspreet <jaspreetkn92@gmail.com>
2024-12-19 08:34:18 +0000

    created data loader to load single job

--- src/pages/JobPage.tsx
+++ src/pages/JobPage.tsx
@@ -1,5 +1,15 @@
+import { useParams } from 'react-router-dom';
+
 const JobPage = () => {
-  return <div>Job Page</div>;
+  const { id } = useParams();
+
+  return <div>{id}</div>;
 };
 
-export default JobPage;
+const jobLoader = async ({ params }) => {
+  const res = await fetch(`http://localhost:8000/jobs/${params.id}`);
+  const data = await res.json();
+  return data;
+};
+
+export { JobPage as default, jobLoader };
