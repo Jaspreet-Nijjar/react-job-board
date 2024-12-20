3c96463729aebe37d2c7eb9fe6e265625cd9412c
Jaspreet <jaspreetkn92@gmail.com>
2024-12-20 08:42:31 +0000

    created the addJob function

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,23 @@ import NotFound from './pages/NotFound';
 import JobPage, { jobLoader } from './pages/JobPage';
 
 const App = () => {
+  const addJob = async (newJob) => {
+    const res = await fetch('http://localhost:8000/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Homepage />} />
         <Route path="/jobs" element={<JobsPage />} />
-        <Route path="/add-job" element={<AddJobPage />} />
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
         <Route path="*" element={<NotFound />} />
         <Route loader={jobLoader} path="/jobs/:id" element={<JobPage />} />
       </Route>
