5dbc83c7b49d49046e9fe6c905ef264e0b55f619
Jaspreet <jaspreetkn92@gmail.com>
2024-12-20 08:22:12 +0000

    added state to all input fields

--- src/pages/AddJobPage.tsx
+++ src/pages/AddJobPage.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 const AddJobPage = () => {
   const [title, setTitle] = useState('');
   const [type, setType] = useState('');
-  const [location, setlocation] = useState('');
+  const [location, setLocation] = useState('');
   const [description, setDescription] = useState('');
   const [salary, setSalary] = useState('');
   const [companyName, setCompanyName] = useState('');
@@ -20,13 +20,15 @@ const AddJobPage = () => {
 
             <div className="mb-4">
               <label
-                htmlhtmlFor="type"
+                htmlFor="type"
                 className="block text-gray-700 font-bold mb-2"
               >
                 Job Type
               </label>
               <select
                 id="type"
+                value={type}
+                onChange={(e) => setType(e.target.value)}
                 name="type"
                 className="border rounded w-full py-2 px-3"
                 required
@@ -45,6 +47,8 @@ const AddJobPage = () => {
               <input
                 type="text"
                 id="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
                 name="title"
                 className="border rounded w-full py-2 px-3 mb-2"
                 placeholder="eg. Beautiful Apartment In Miami"
@@ -61,6 +65,8 @@ const AddJobPage = () => {
               <textarea
                 id="description"
                 name="description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
                 className="border rounded w-full py-2 px-3"
                 rows="4"
                 placeholder="Add any job duties, expectations, requirements, etc"
@@ -77,6 +83,8 @@ const AddJobPage = () => {
               <select
                 id="salary"
                 name="salary"
+                value={salary}
+                onChange={(e) => setSalary(e.target.value)}
                 className="border rounded w-full py-2 px-3"
                 required
               >
@@ -102,6 +110,8 @@ const AddJobPage = () => {
                 type="text"
                 id="location"
                 name="location"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
                 className="border rounded w-full py-2 px-3 mb-2"
                 placeholder="Company Location"
                 required
@@ -120,6 +130,8 @@ const AddJobPage = () => {
               <input
                 type="text"
                 id="company"
+                value={companyName}
+                onChange={(e) => setCompanyName(e.target.value)}
                 name="company"
                 className="border rounded w-full py-2 px-3"
                 placeholder="Company Name"
@@ -135,6 +147,8 @@ const AddJobPage = () => {
               </label>
               <textarea
                 id="company_description"
+                value={companyDescription}
+                onChange={(e) => setCompanyDescription(e.target.value)}
                 name="company_description"
                 className="border rounded w-full py-2 px-3"
                 rows="4"
@@ -153,6 +167,8 @@ const AddJobPage = () => {
                 type="email"
                 id="contact_email"
                 name="contact_email"
+                value={contactEmail}
+                onChange={(e) => setContactEmail(e.target.value)}
                 className="border rounded w-full py-2 px-3"
                 placeholder="Email address htmlFor applicants"
                 required
@@ -169,6 +185,8 @@ const AddJobPage = () => {
                 type="tel"
                 id="contact_phone"
                 name="contact_phone"
+                value={contactPhone}
+                onChange={(e) => setContactPhone(e.target.value)}
                 className="border rounded w-full py-2 px-3"
                 placeholder="Optional phone htmlFor applicants"
               />
